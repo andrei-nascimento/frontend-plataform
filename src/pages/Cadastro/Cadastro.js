9bee802bfe735a28aff3006219b7dd344fc31ac6
Andrei Nascimento <andrei.lima.nas@outlook.com>
2023-04-06 18:07:50 -0300

    Divisão dos conteúdos na tela de cadastro

--- src/pages/Cadastro/Cadastro.js
+++ src/pages/Cadastro/Cadastro.js
@@ -1,6 +1,7 @@
 import React from "react";
 import waveBg from '../../assets/imgs/wave-bg.png';
 import logo from '../../assets/imgs/logo.png';
+import './Cadastro.css';
 
 function Cadastro() {
 
@@ -9,78 +10,65 @@ function Cadastro() {
             <div className="row">
 
                 <div className="col col-6 border p-0">
-                    <img src={waveBg} className="vh-100 w-100" alt="intro-image"/>
+                    <img src={waveBg} className="vh-100 w-100" alt="background"/>
                 </div>
 
                 <div className="col col-6 border p-0">
-                    <div style={{
-                    marginTop: '80px', 
-                    marginLeft: '40px', 
-                    marginBottom: '40px'
-                    }}>
-                        <img src={logo} alt="logotipo" />
-                    </div>
-
-                    <div style={{ 
-                    marginLeft: '40px', 
-                    marginBottom: '28px',
-                    color: '#476EE6',
-                    fontSize: '24px',
-                    fontWeight: '500'
-                    }}>
-                        Cadastro
+                    <div className="headerCadastro">
+                        <img src={logo} alt="logotipo" className="logotipo"/>
+                        <p className="titleCadastro">Cadastro</p>
                     </div>
 
                     <form>
-                        <div className="row" style={{marginLeft: '32px', marginRight: '32px'}}>
-
-                            <div className="col-md-6 p-8">
-                                <div className="form-group" style={{marginBottom: '26px'}}>
-                                    <label for="inputNome" style={{
-                                    fontSize: '14px', 
-                                    marginBottom: '12px', 
-                                    fontWeight: '500'
-                                    }}>
-                                        Nome
-                                    </label>
-                                    <input type="text" className="form-control h-50" id="inputNome" placeholder="Digite seu nome" />
+                        <div className="formCadastro">
+                            <div className="boxNomeSenha">
+                                <div className="form-group-nome">
+                                    <label for="inputNome" className="labelCadastro">Nome</label>
+                                    <input type="text" className="form-control" id="inputNome" placeholder="Digite seu nome" />
                                 </div>
 
-                                <div className="form-group">
-                                    <label for="inputSenha" style={{
-                                    fontSize: '14px', 
-                                    marginBottom: '12px', 
-                                    fontWeight: '500'
-                                    }}>
-                                        Senha
-                                    </label>
+                                <div className="form-group-senha">
+                                    <label for="inputSenha" className="labelCadastro">Senha</label>
                                     <input type="text" className="form-control" id="inputSenha" placeholder="Digite sua senha" />
                                 </div>
                             </div>
 
-                            <div className="col-md-6 p-8">
-                                <div className="form-group" style={{marginBottom: '26px'}}>
-                                    <label for="inputEmail" style={{
-                                    fontSize: '14px', 
-                                    marginBottom: '12px', 
-                                    fontWeight: '500'}}>
-                                        E-mail
-                                    </label>
+                            <div className="boxEmailSenha">
+                                <div className="form-group-email">
+                                    <label for="inputEmail" className="labelCadastro">E-mail</label>
                                     <input type="text" className="form-control" id="inputEmail" placeholder="Digite seu e-mail" />
                                 </div>
 
-                                <div class="form-group">
-                                    <label for="inputConfirmaSenha" style={{
-                                    fontSize: '14px', 
-                                    marginBottom: '12px', 
-                                    fontWeight: '500'}}>
-                                        Confirme sua senha
-                                    </label>
+                                <div className="form-group-confirma-senha">
+                                    <label for="inputConfirmaSenha" className="labelCadastro">Confirme sua senha</label>
                                     <input type="text" className="form-control" id="inputConfirmaSenha" placeholder="Confirma senha" />
                                 </div>
                             </div>
                         </div>
                     </form>
+
+                    <div className="boxTermos">
+                        <p>Termos de uso e privacidade</p>
+                        <div>
+                            <input type="checkbox" className="iptCheck"/>
+                            <p>Ao clicar neste botão, eu concordo com os termos de uso e privacidade da nossa empresa</p>
+                        </div>
+                        <p>
+                            <a href="#">Termos de uso e Privacidade</a>
+                        </p>
+                    </div>
+
+                    <div className="btnCadastrar">
+                        <p className="textCadastrar">Cadastrar</p>
+                    </div>
+
+                    <div className="boxLinkLogin">
+                        <p>Já tem uma conta?</p>
+                        <p>
+                            <a href="#">Login</a>
+                        </p>
+                    </div>
+
                 </div>
             </div>
         </div>
